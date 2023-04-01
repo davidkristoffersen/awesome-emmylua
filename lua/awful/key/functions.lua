bd2e15e4bff8c7a43d677e7ebf8e1247887af42a
David Kristoffersen <david.alta2010@gmail.com>
2023-04-01 10:48:03 +0200

    Add basic awful.key functions

--- lua/awful/key/functions.lua
+++ lua/awful/key/functions.lua
@@ -6,6 +6,8 @@ local ak
 --- ### Description
 --- Awful key is a module for keybindings management in AwesomeWM.
 --- It provides a simple way to define keybindings and associate them with actions.
+--- ### Constructor
+--- Returns `AKey[]`, see `awful.key.new`.
 ---
 --- ### Examples
 --- You can use the `awful.key` module to create keybindings for various actions. For example, you can define keybindings to switch between tags:
@@ -21,10 +23,10 @@ local ak
 --- You can also define keybindings to control window layouts, clients, and more.
 ---
 --- [View documents](https://awesomewm.org/apidoc/input_handling/awful.key.html)
---- @class awful.key
+--- @class AKey
 local AKey = ak
 
---- @alias Modifier
+--- @class Modifier
 --- | '"Any"' # Any modifier key.
 --- | '"Mod1"' # Alt key.
 --- | '"Mod2"' # Num Lock key.
@@ -35,14 +37,17 @@ local AKey = ak
 --- | '"Lock"' # Caps Lock key.
 --- | '"Control"' # Control key.
 
---- Create a key object..
---- @constructor
---- @param mod Modifier[] A table with modifier keys.
---- @param _key string A key name.
---- @param press function A function to call when the key is pressed.
+--- ### Description
+--- Create a new key object
+---
+--- ### Parameters
+--- @param mod Modifier[]|nil A table with modifier keys.
+--- @param _key string|nil A key name.
+--- @param press function|nil A function to call when the key is pressed.
 --- @param release function? A function to call when the key is released.
 --- @param data table? A table with extra data.
---- @return awful.key[]: A table with key objects.
+--- ### Returns
+--- @return AKey[]: A table with key objects.
 function AKey.new(mod, _key, press, release, data) end
 
 return AKey
